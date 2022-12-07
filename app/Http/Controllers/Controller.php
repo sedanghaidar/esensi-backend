298650be3780f68c85920061c41696c7fc56e7a3
Anaz S. Aji <aji.anaz@gmail.com>
2022-12-07 11:36:49 +0700

    set up controller untuk respon api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,36 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    private function responseJson($success, $statusCode, $message, $result)
+    {
+        $data =  array(
+            'success' => $success,
+            'code' => $statusCode,
+            'message' => $message,
+            'result' => $result
+        );
+
+        return response()->json($data, $statusCode);
+    }
+
+    public function success($message, $data)
+    {
+        return $this->responseJson(true, 200, $message, $data);
+    }
+
+    public function error($message)
+    {
+        return $this->responseJson(false, 400, $message, null);
+    }
+
+    public function unauthorized($message)
+    {
+        return $this->responseJson(false, 401, $message, null);
+    }
+
+    public function invalidation($message)
+    {
+        return $this->responseJson(false, 400, $message, null);
+    }
 }
