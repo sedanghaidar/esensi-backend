634639d695f1c0180b06d3376e4fd5d1012ab87b
Anaz S. Aji <aji.anaz@gmail.com>
2022-12-16 11:27:45 +0700

    tambah kolom pada table participants dan organizations

---
+++ database/migrations/2022_12_16_105818_add_id_organisasi_to_participant_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIdOrganisasiToParticipantTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('participants', function (Blueprint $table) {
+            $table->integer('organization_id')->after('instansi')->nullable(false)->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('participants', function (Blueprint $table) {
+            //
+        });
+    }
+}
