7c147406d8fb0840df2fb438bfc861a3e4776c37
hanifabdullah <hanifabdullah7@gmail.com>
2024-01-20 18:46:08 +0700

    try add cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://cs.saturnalia.jatengprov.go.id'],
 
     'allowed_origins_patterns' => [],
 
